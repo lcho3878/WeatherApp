8e7bf7c7174050e91a5c700b3d04617ad41740c3
lcho3878 <leech3878@naver.com>
2024-06-22 14:27:04 +0900

    Feat: 위치 사용 권한 관련 함수 작성

--- WeatherApp/MainViewController.swift
+++ WeatherApp/MainViewController.swift
@@ -6,14 +6,94 @@
 //
 
 import UIKit
+import CoreLocation
+import SnapKit
 
 class MainViewController: UIViewController {
 
+    private let locationManager = CLLocationManager()
+    
+    private let cityLabel = {
+        let lb = UILabel()
+        lb.textColor = .white
+        return lb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
+        configurelocationManager()
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    private func configureUI() {
         view.backgroundColor = .systemOrange
     }
+    
+    private func configureHierarchy() {
+        view.addSubview(cityLabel)
+    }
+    
+    private func configureLayout() {
+        cityLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    
+    }
 
+}
 
+extension MainViewController: CLLocationManagerDelegate {
+    
+    private func configurelocationManager() {
+        locationManager.delegate = self
+    }
+    
+    private func checkDeiveLocationAuthorization() {
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled() {
+                self.checkCurrentLocationAuthorization()
+            }
+            else {
+                print("위치 서비스 꺼져있음")
+            }
+        }
+        
+    }
+    
+    private func checkCurrentLocationAuthorization() {
+        let status = locationManager.authorizationStatus
+        
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .denied:
+            print("거부")
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            print(status)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let lat = manager.location?.coordinate.latitude,
+              let lon = manager.location?.coordinate.longitude else { return }
+        locationManager.stopUpdatingLocation()
+        cityLabel.text = "lat: \(lat), lon: \(lon)"
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print(#function)
+    }
+    
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+//        print(#function)
+        checkDeiveLocationAuthorization()
+    }
+    
 }
 
